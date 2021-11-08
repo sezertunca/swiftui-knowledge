6e7094073ba7fa7438fffb733ddfe274aa30c7eb
Sezer Tunca <sezertunca@gmail.com>
2021-11-08 13:06:53 +0000

    Initial Commit

---
+++ SwiftUIKnowledge/SwiftUIKnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKnowledgeApp.swift
+//  SwiftUIKnowledge
+//
+//  Created by Sezer Tunca on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
