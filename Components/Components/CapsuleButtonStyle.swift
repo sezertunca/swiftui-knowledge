8a2c433ca8593e7c691e81fbad29dd61a6571e27
Sezer Tunca <sezertunca@gmail.com>
2021-11-24 06:54:39 +0000

    Add SearchView

---
+++ Components/Components/CapsuleButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  CapsuleButtonStyle.swift
+//  Components
+//
+//  Created by Sezer Tunca on 19/11/2021.
+//
+
+import Foundation
---
+++ Components/Components/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  Components
+//
+//  Created by Sezer Tunca on 18/11/2021.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}
